e056d2ad51cc77eaa9b771b59d34251e262a17a8
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-10-19 13:48:23 -0700

    crop player sprite and center on screen

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = '/Users/andrewb/Desktop/pokeGame/img/pokeGame Map.png'
 
+const playerImage = new Image();
+playerImage.src = '/Users/andrewb/Desktop/pokeGame/img/playerDown.png';
+
+
 image.onload = () => {
     c.drawImage(image, -1100, -700, );
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+    canvas.width / 2 - (playerImage.width / 4) / 2, 
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+    );
 }
+
+
