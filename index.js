ca58ce7847c0841c719029954ebc3aece95ed604
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-10-19 20:42:20 -0700

    set animation frame on loop to begin programming character movement

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ image.src = '/Users/andrewb/Desktop/pokeGame/img/pokeGame Map.png'
 const playerImage = new Image();
 playerImage.src = '/Users/andrewb/Desktop/pokeGame/img/playerDown.png';
 
-
+//placing and centering player on loaded center screen
 image.onload = () => {
     c.drawImage(image, -1100, -700, );
     c.drawImage(
@@ -30,4 +30,39 @@ image.onload = () => {
     );
 }
 
+//player movement
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.drawImage(image, -1100, -700, );
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+    canvas.width / 2 - (playerImage.width / 4) / 2, 
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+    );
+    console.log('animate');
+}
+animate();
 
+
+window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+        case 'w':
+            console.log("w")
+            break;
+        case 'a':
+            console.log("a")
+            break;
+        case 'd':
+            console.log("d")
+            break;
+        case 's':
+            console.log("s")
+            break;
+    }
+})
