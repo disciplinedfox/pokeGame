0d57668219bcb54bc0c82fe1d1b8bdf7694feb57
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-10-22 17:06:12 -0700

    add sprite 4 direction movement

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Sprite {
         this.image = image
     }
     draw() {
-        c.drawImage(this.image, -1100, -700, );
+        c.drawImage(this.image, this.position.x, this.position.y);
     }
 }
 
@@ -57,13 +57,16 @@ function animate() {
         0,
         playerImage.width / 4,
         playerImage.height,
-    canvas.width / 2 - (playerImage.width / 4) / 2, 
+    canvas.width / 2 - playerImage.width / 4 / 2, 
     canvas.height / 2 - playerImage.height / 2,
     playerImage.width / 4,
     playerImage.height
     );
-    console.log('animate');
-}
+    if (keys.w.pressed) background.position.y += 3
+    else if (keys.a.pressed) background.position.x += 3
+    else if (keys.s.pressed) background.position.y -= 3
+    else if (keys.d.pressed) background.position.x -= 3
+    }
 animate();
 
 
@@ -82,4 +85,23 @@ window.addEventListener('keydown', (e) => {
             keys.s.pressed = true;
             break;
     }
+    console.log(keys)
+})
+
+window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+    }
+    console.log(keys)
 })
